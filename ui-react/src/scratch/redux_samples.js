7a7043f89d1bff56531336a19e963e0a50ca0e61
merrillpaul <merrill.paul@pearson.com>
2017-01-22 20:35:24 -0600

    Playing with redux promises middleware

--- ui-react/src/scratch/redux_samples.js
+++ ui-react/src/scratch/redux_samples.js
@@ -1,11 +1,12 @@
 import {createStore, combineReducers, applyMiddleware} from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
+import promise from 'redux-promise-middleware';
 import reducers from './reducers'
 import axios from 'axios';
 
 const _reducers = combineReducers(reducers);
-const middleWare = applyMiddleware(thunk, logger());
+const middleWare = applyMiddleware(promise(), thunk, logger());
 const store = createStore(_reducers, middleWare);
 
 export function redux() {
@@ -36,5 +37,33 @@ export function redux() {
       }).catch((err) => {
         dispatch({type: 'FETCH_USERS_ERROR', payload: err});
       });
+    });
+
+
+    // via promises much cleaner, look at the console as it has some default
+    // action types
+    /*
+    action @ 20:31:35.325 FETCH_USERS_VIA_PROMISES_PENDING
+    core.js:111  prev state Object {user: Object, token: Object}
+    core.js:115  action Object {type: "FETCH_USERS_VIA_PROMISES_PENDING"}
+    core.js:123  next state Object {user: Object, token: Object}
+
+    then after the xhr returns
+    action @ 20:31:36.553 FETCH_USERS_VIA_PROMISES_FULFILLED
+    core.js:111  prev state Object {user: Object, token: Object}
+    core.js:115  action Object {type: "FETCH_USERS_VIA_PROMISES_FULFILLED", payload: Object}
+    core.js:123  next state Object {user: Object, token: Object}
+
+    if there is an error
+    action @ 20:33:36.189 FETCH_USERS_VIA_PROMISES_REJECTED
+    core.js:111  prev state Object {user: Object, token: Object}
+    core.js:115  action Object {type: "FETCH_USERS_VIA_PROMISES_REJECTED", payload: Error: Network Error
+        at createError (http://localhost:3000/static/js/bundle.js:61418:16)
+        at …, error: true}
+    core.js:123  next state Object {user: Object, token: Object}
+    */
+    store.dispatch({
+      type: 'FETCH_USERS_VIA_PROMISES',
+      payload: axios.get('http://rest.learncode.academy/api/wstern/users')
     });
 }
